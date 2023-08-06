401f37886e8b678a30f8dcbf38b4586b21fe6fb4
jlarrayoz <juan.larrayoz@gmail.com>
2023-08-05 21:19:32 -0300

    Commit inicial del proyecto

---
+++ src/main/java/uy/org/curso/SpringMvcInicialApplication.java
@@ -0,0 +1,13 @@
+package uy.org.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcInicialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcInicialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uy/org/curso/SpringMvcInicialApplicationTests.java
@@ -0,0 +1,13 @@
+package uy.org.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcInicialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
